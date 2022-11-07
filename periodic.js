9808f2207402ba52f98199435c25e98fedc2b8cc
“DhruvKudale” <dkudale07@gmail.com>
2022-11-07 23:36:23 +0530

    Adding description table details

--- periodic.js
+++ periodic.js
@@ -11,5 +11,16 @@ function loadTileDetails(i) {
     document.getElementById("desc-state").innerHTML = data['StandardState'];
     document.getElementById("desc-atomic-mass").innerHTML = data['AtomicMass'];
     document.getElementById("desc-electronic-configuration").innerHTML = data['ElectronConfiguration'];
+    document.getElementById("desc-melting-point").innerHTML = data['MeltingPoint'];
+    document.getElementById("desc-boiling-point").innerHTML = data['BoilingPoint'];
+    document.getElementById("desc-year").innerHTML = data['YearDiscovered'];
  	document.getElementById("desc-table").removeAttribute("hidden")
+}
+
+function getColourFromBlock(block) {
+   return "black";
+}
+
+function displayTrends() {
+   color = getColourFromBlock(block);
 }
