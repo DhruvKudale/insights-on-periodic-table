93a61adb7be9d987991a69361210d4a01a151f99
“DhruvKudale” <dkudale07@gmail.com>
2022-11-08 12:54:38 +0530

    Background color changing for standard state

--- periodic.js
+++ periodic.js
@@ -21,6 +21,22 @@ function getColourFromBlock(block) {
    return "black";
 }
 
+function getColourFromState(state) {
+   if(state == "Solid")
+      return "#ed8a8a";
+   else if(state == "Liquid")
+      return "#73a3f0";
+   else if(state == "Gas")
+      return "#b8f073";
+   else 
+      return "#c7c3c3";
+}
+
 function displayTrends() {
-   color = getColourFromBlock(block);
+   for(i=1; i<=118; i++) {
+      data = elementsdata[i-1];
+      elementid = "z" + String(i);
+      color = getColourFromState(data['StandardState']);
+      document.getElementById(elementid).style.backgroundColor = color;
+   }
 }
