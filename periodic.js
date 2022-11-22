7c18ad56b678ff73c2356cfe87c842a35fae4d5b
“DhruvKudale” <dkudale07@gmail.com>
2022-11-22 15:33:53 +0530

    Adding element wise page

--- periodic.js
+++ periodic.js
@@ -95,3 +95,33 @@ function displayTrends() {
       }
    }
 }
+
+function loadElementData() {
+   element = document.getElementById("ele-dropdown").value
+   id = Number(element);
+   data = elementsdata[id - 1];
+   document.getElementById("ele-name").innerHTML = data['Element Name'];
+   document.getElementById("ele-z").innerHTML = data['Element Atomic Number'];
+   document.getElementById("ele-symbol").innerHTML = data['Element Symbol'];
+   document.getElementById("ele-state").innerHTML = data['phase'];
+   document.getElementById("ele-atomic-mass").innerHTML = data['Element Atomic Weight'];
+   document.getElementById("ele-electronic-configuration").innerHTML = data['ElectronConfiguration'];
+   document.getElementById("ele-melting-point").innerHTML = data['Element Absolute Melting Point'];
+   document.getElementById("ele-boiling-point").innerHTML = data['Element Absolute Boiling Point'];
+   document.getElementById("ele-category").innerHTML = data['Element Category'];
+   document.getElementById("ele-year").innerHTML = data['Element Year of Discovery'];
+   document.getElementById("ele-ionization-energy").innerHTML = data['Element Ionization Energies'];
+   document.getElementById("ele-abundance").innerHTML = data['Element Abundance in Earth\'s Crust'];
+   document.getElementById("ele-color").innerHTML = data['Element Color'];
+   document.getElementById("ele-block").innerHTML = data['Element Block'];
+   document.getElementById("ele-density").innerHTML = data['Element Density'];
+   document.getElementById("ele-valence").innerHTML = data['Element Valence'];
+   document.getElementById("ele-affinity").innerHTML = data['Element Electron Affinity'];
+   document.getElementById("ele-negativity").innerHTML = data['Element Electronegativity (Pauling)'];
+   document.getElementById("ele-country").innerHTML = data['Element Country of Discovery']; 
+   document.getElementById("ele-discovered").innerHTML = data['Discovered_by'];
+   document.getElementById("ele-summary").innerHTML = data['summary'];
+   document.getElementById("ele-model").setAttribute("src", data['bohr_model_3d']);
+   document.getElementById("ele-model").removeAttribute("hidden");
+ 	document.getElementById("ele-table").removeAttribute("hidden");
+}
